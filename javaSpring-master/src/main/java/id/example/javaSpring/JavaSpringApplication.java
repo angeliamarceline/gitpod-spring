db7599e2d3e44dd0c0148271e660d3f8110e8955
Angelia Marceline <angelia.marceline@bankmandiri.co.id>
2021-08-24 15:14:52 +0700

    on going

--- javaSpring-master/src/main/java/id/example/javaSpring/JavaSpringApplication.java
+++ javaSpring-master/src/main/java/id/example/javaSpring/JavaSpringApplication.java
@@ -8,11 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableBatchProcessing
 public class JavaSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringApplication.class, args);
-	}
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(JavaSpringApplication.class, args);
+	// }
 
-//	public static void main(String[] args) throws Exception {
-//		System.exit(SpringApplication.exit(SpringApplication.run(JavaSpringApplication.class, args)));
-//	}
+	public static void main(String[] args) throws Exception {
+		System.exit(SpringApplication.exit(SpringApplication.run(JavaSpringApplication.class, args)));
+	}
 }
